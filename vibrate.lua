ca190fe39b6930b87683e73987ccfa99a4ae6626
Anne Jan Brouwer <brouwer@annejan.com>
2015-02-22 15:44:57 +0100

    flag

--- vibrate.lua
+++ vibrate.lua
@@ -1,12 +1,18 @@
 n = 0
+vibrate = false
 
 gpio.mode(5,gpio.OUTPUT)
 
 tmr.alarm(3, 500, 1, function() 
-	if n % 2 == 0 then
-		gpio.write(5,gpio.HIGH)
+
+	if vibrate then
+		if n % 2 == 0 then
+			gpio.write(5,gpio.HIGH)
+		else
+			gpio.write(5,gpio.LOW)
+		end
+		n = n + 1
 	else
-		gpio.write(5,gpio.LOW)
+		n = 0
 	end
-	n = n + 1
 end)
