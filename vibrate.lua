04f20a846e0459f1eb8963fe0176b859b5563d6f
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2015-02-25 22:19:56 +0100

    test and better vibration test

--- vibrate.lua
+++ vibrate.lua
@@ -3,16 +3,18 @@ vibrate = false
 
 gpio.mode(5,gpio.OUTPUT)
 
-tmr.alarm(3, 500, 1, function() 
-
-	if vibrate then
-		if n % 2 == 0 then
-			gpio.write(5,gpio.HIGH)
-		else
-			gpio.write(5,gpio.LOW)
-		end
-		n = n + 1
-	else
-		n = 0
+vibrate = function (state) 
+	if state == true then 
+		tmr.alarm(3, 500, 1, function() 
+			if n % 2 == 0 then
+				gpio.write(5,gpio.HIGH)
+			else
+				gpio.write(5,gpio.LOW)
+			end
+			n = n + 1
+		end)
+	else 
+		tmr.stop(3)
+		gpio.write(5,gpio.LOW)
 	end
-end)
+end
