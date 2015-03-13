e959b4bb1a7ce5eccd427334e90612d2647ee7d3
Anne Jan Brouwer <brouwer@annejan.com>
2015-03-13 17:15:50 +0100

    chaged to u8glib

--- oled.lua
+++ oled.lua
@@ -1,8 +1,31 @@
-oled.setup(3,4)
-oled.line(1,1,"ijWatch 0.0.1    test")
-oled.line(2,1,"+-------------------+")
-oled.line(2,2,"ijWatch # IJhack")
-oled.line(5,1,"lol 1 2 3 4 5 6 7 8 9")
-oled.line(6,1,"!@#$%^&*()-+\/\"<>?~`;:")
-oled.line(7,1,"+-------------------+")
-oled.line(8,1,"2015 Anne Jan Brouwer")
+sda = 3
+scl = 4
+sla = 0x3c
+
+i2c.setup(0, sda, scl, i2c.SLOW)
+disp = u8g.ssd1306_128x64_i2c(sla)
+
+disp:setFont(u8g.font_6x10)
+disp:setFontRefHeightExtendedText()
+disp:setDefaultForegroundColor()
+disp:setFontPosTop()
+
+function drawAnus()
+	disp:drawStr(0, 0, "ijWatch")
+end
+
+function drawCircle()
+     disp:drawStr(0, 0, "ijWatch 0.1.8")
+     disp:drawCircle(10, 18+30, 9)
+     disp:drawCircle(24, 16+30, 7)
+end
+
+disp:firstPage()
+repeat
+	drawAnus()
+until disp:nextPage() == false
+
+disp:firstPage()
+repeat
+	drawCircle()
+until disp:nextPage() == false
